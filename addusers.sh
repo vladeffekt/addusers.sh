8ddcada93b89444a4d89585985214c90fcb8be68
Vladislav <115285754+vladeffekt@users.noreply.github.com>
2025-06-06 09:51:12 +0300

    Update addusers.sh

--- addusers.sh
+++ addusers.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
 users=`awk '{print $1}' $1
-
 for lusers in $users
 do
 echo $lusers | xargs -n 1 -p useradd -s /bin/bash -m
 done
-
 cat $1 | chpasswd
-
 #check
 tail -n 3 /etc/passwd #users was create
 tail -n 3 /etc/passwd | awk -F: '{print $1}' | xargs -n 1 passwd -S   #password was create
